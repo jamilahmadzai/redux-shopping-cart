f1392ef22d69479bbacb4bba09f3255d41bde99c
Jamil Ur Rehman Ahmadzai <jamilurrehman722@gmail.com>
2021-01-22 03:38:55 +0100

    set boiler plate of the project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
+// redux stuff
+import { createStore } from "redux";
+
+const initailState = {
+  count: 0,
+};
+
+const reducer = (state = initailState, action) => {
+  console.log(state, action);
+};
+const store = createStore(reducer);
+
+function App() {
+  // cart setup
+
+  return (
+    <main>
+      <Navbar />
+      <CartContainer cart={cartItems} />
+    </main>
+  );
+}
+
+export default App;
